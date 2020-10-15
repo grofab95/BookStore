04633380e14501e031e91dc3f4d9e3eca980005d
Fabian <grochla95@gmail.com>
2020-10-16 01:34:43 +0200

    Add IBookDao and start try mocking

---
+++ BookStore.Domain/Adapters/IBookDao.cs
@@ -0,0 +1,15 @@
+﻿using BookStore.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BookStore.Domain.Adapters
+{
+    public interface IBookDao
+    {
+        Task<int> AddBookAsync(Book book);
+        Task<Book> GetBookById(int bookId);
+        Task<List<Book>> GetBooksAsync(int offset, int limit);
+        Task<Book> UpdateBookAsync(Book book);
+        Task RemoveBookAsync(Book book);
+    }
+}
