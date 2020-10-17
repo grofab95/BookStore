ff5b9826f1e57fc9d3f4545865fb0c6e3b6792ae
Fabian <grochla95@gmail.com>
2020-10-17 13:41:02 +0200

    add customer tests and add customer

--- BookStore.Domain.Tests/Entities/CustomerTests.cs
+++ BookStore.Domain.Tests/Entities/CustomerTests.cs
@@ -1,7 +1,18 @@
-﻿namespace BookStore.Domain.Tests.Entities
+﻿using BookStore.Domain.Entities;
+using FluentAssertions;
+using Xunit;
+
+namespace BookStore.Domain.Tests.Entities
 {
     public class CustomerTests
     {
+        [Fact]
+        public void Customer_Should_Created()
+        {
+            var address = new Address("Wrocław", "Oławska", "12-345", 50, 2);
+            var customer = new Customer("Fabian", "Grochla", "fabian.grochla@gmail.com", address);
 
+            customer.Should().NotBeNull();
+        }
     }
 }
---
+++ BookStore.Domain/Entities/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace BookStore.Domain.Entities
+{
+    public class Customer : User
+    {
+        public Customer(string firstName, string lastName, string email, Address addresss) 
+            : base(firstName, lastName, email, addresss)
+        {
+
+        }
+    }
+}
